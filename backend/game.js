91051f7edb5d4d6d81e8059df41df1f3cdaddad0
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-10-20 19:42:50 +0400

    Add game.js

---
+++ backend/game.js
@@ -0,0 +1,7 @@
+// game.js
+class Game {
+  constructor(){ /* init board array */ }
+  validateMove(src, dst){ /* return true/false */ }
+  applyMove(src, dst){ /* update board */ }
+}
+module.exports = Game;
